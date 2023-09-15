ce4d4e544b21ba9e7dfed05642411e9f35bd6289
Bryan Mey <bryanmey@Bryans-MacBook-Pro-2.local>
2023-09-15 03:01:55 -0400

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bmey.github.io',
+  site: 'http://luckystone.band',
   integrations: [react(), tailwind()]
 });
