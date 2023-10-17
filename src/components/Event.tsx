a4248408f68c5224e7ddd0647923a277568d67d8
Bryan Mey <bryan.j.mey@gmail.com>
2023-10-17 15:23:44 -0400

    update events, convert event to react component to eval if should be hidden or not

---
+++ src/components/Event.tsx
@@ -0,0 +1,33 @@
+import dayjs from 'dayjs';
+
+const yesterday = dayjs().subtract(1, "day");
+
+type DateString = `${number}${number}${number}${number}-${number}${number}-${number}${number}`;
+
+export interface EventProps {
+    date: DateString;
+    facebookEventId: string;
+    locationName: string;
+}
+
+export const Event = (props: EventProps) => {
+    const { date, facebookEventId, locationName } = props;
+    const formattedDate = dayjs(date).format("dddd, MMM D");
+    
+    if (!yesterday.isBefore(date)) {
+        return null;
+    }
+
+    return (
+        <a className="event" href={`https://www.facebook.com/events/${facebookEventId}/`}>
+            <div className="header date">
+                <div>{formattedDate}</div>
+            </div>
+            <div className="header">
+                <i className="fa-solid fa-location-dot"></i>
+                <div>{locationName}</div>
+            </div>
+            <div className="link">View more</div>
+        </a>
+    );
+};
